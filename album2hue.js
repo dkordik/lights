c7ceb9dc7e27de808ad5b623d5dd8f96f0cab283
Dan Kordik <dkordik.public@gmail.com>
2013-10-01 00:26:10 -0500

    One awesome node lib later, hooked album2hue up to iTunes playback event

--- album2hue.js
+++ album2hue.js
@@ -2,14 +2,16 @@
 
 var exec = require('child_process').exec;
 
-var HueApi = require("node-hue-api").HueApi;
-
 var hostname = "192.168.2.142";
 var username = "newdeveloper";
+var HueApi = require("node-hue-api").HueApi;
 var api = new HueApi(hostname, username);
 
 var colorConverter = require("./colorconverter").colorConverter();
 
+var playback = require('playback');
+
+//the node.js community is amazing... so many great libraries made my job easy.
 
 var lights = function (which, request) {
 	LIGHTS[which].ids.forEach(function (light) {
@@ -47,30 +49,37 @@ var LIGHTS = {
 	KITCHEN: { ids: [ 4, 5, 6 ], bri: 0 }
 }
 
-applescript("getItunesCover.applescript", function () {
-	rb("getDominantCoverColors.rb", function (colors) {
+var updateLightsToAlbum = function () {
+	applescript("getItunesCover.applescript", function () {
+		rb("getDominantCoverColors.rb", function (colors) {
 
-		colors = colors.filter(function (rgb) {
-			var max = 255;
-			var thresh = max / 2;
+			colors = colors.filter(function (rgb) {
+				var max = 255;
+				var thresh = max / 2;
 
-			atleastOneLight = rgb[0] > thresh || rgb[1] > thresh || rgb[2] > thresh;
-			atleastOneDark = rgb[0] < thresh || rgb[1] < thresh || rgb[2] < thresh;
+				atleastOneLight = rgb[0] > thresh || rgb[1] > thresh || rgb[2] > thresh;
+				atleastOneDark = rgb[0] < thresh || rgb[1] < thresh || rgb[2] < thresh;
 
-			return atleastOneLight && atleastOneDark;
+				return atleastOneLight && atleastOneDark;
+			});
+
+			var bestColor = colors[0]; //TODO: smarter bestColor picking
+
+			var rgb = { r: bestColor[0]/255, g: bestColor[1]/255, b: bestColor[2]/255 };
+
+			var color = colorConverter.rgbToXyBri(rgb);
+			var color = colorConverter.xyBriForModel(color, 'LCT001');
+
+			lights("OFFICE", {
+				bri: Math.round(color.bri * 255),
+				xy: [ color.x, color.y ]
+			});
+
 		});
-
-		var bestColor = colors[0]; //TODO: smarter bestColor picking
-
-		var rgb = { r: bestColor[0]/255, g: bestColor[1]/255, b: bestColor[2]/255 };
-
-		var color = colorConverter.rgbToXyBri(rgb);
-		var color = colorConverter.xyBriForModel(color, 'LCT001');
-
-		lights("OFFICE", {
-			bri: 180, //Math.round(color.bri * 255),
-			xy: [ color.x, color.y ]
-		});
-
 	});
+}
+
+playback.on('playing', function(data) {
+	console.dir(data);
+	updateLightsToAlbum();
 });
