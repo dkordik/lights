2fe5df023ac02e3876fd0a1221d4cda52987bbc9
Dan Kordik <dkordik.public@gmail.com>
2013-10-10 18:58:56 -0500

    Fix process.exit

--- getAppleCoverURL.js
+++ getAppleCoverURL.js
@@ -7,22 +7,22 @@ var artist = process.argv[2];
 var album = process.argv[3];
 
 google("site:itunes.apple.com inurl:album " + album + " by " + artist, function(err, next, links){
-	if (err) { console.error(err); exit(1); }
+	if (err) { console.error(err); process.exit(1); }
 	if (!links) {
 		console.error("No google results.");
-		exit(1);
+		process.exit(1);
 	}
 	scraper(links[0].href, function(err, jQuery) {
-		if (err) { console.error(err); exit(1); }
+		if (err) { console.error(err); process.exit(1); }
 
 		var artworkEl = jQuery(".artwork[width=170]");
 		if (artworkEl.length != 0) {
 			var bigArtURL = artworkEl[0].src.replace("170x170", "1200x1200");
 			console.log('{ "url": "' + bigArtURL + '" }');
-			exit(0);
+			process.exit(0);
 		} else {
 			console.error("No artwork found on Apple page with width of 170px");
-			exit(1);
+			process.exit(1);
 		}
 	});
 });
