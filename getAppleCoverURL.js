fa96f85651bd3b9c2bed86631913241cb8482d9e
Dan Kordik <dkordik.public@gmail.com>
2013-10-14 20:09:54 -0500

    Update getAppleCoverURL to use google search API instead of scraping google search

--- getAppleCoverURL.js
+++ getAppleCoverURL.js
@@ -1,28 +1,51 @@
 #!/usr/bin/env node
 
-var google = require('google');
-var scraper = require('scraper');
+var config = require("./.config.json");
 
 var artist = process.argv[2];
 var album = process.argv[3];
 
-google("site:itunes.apple.com inurl:album " + album + " by " + artist, function(err, next, links){
-	if (err) { console.error(err); process.exit(1); }
-	if (!links) {
-		console.error("No google results.");
-		process.exit(1);
-	}
-	scraper(links[0].href, function(err, jQuery) {
-		if (err) { console.error(err); process.exit(1); }
+var googleapis = require('googleapis');
+var scraper = require('scraper');
+
+var scrapeBigArtwork = function (pageURL) {
+	scraper(pageURL, function(err, jQuery) {
+		if (err) {
+			console.error(err);
+			process.exit(1);
+		}
 
 		var artworkEl = jQuery(".artwork[width=170]");
+
 		if (artworkEl.length != 0) {
 			var bigArtURL = artworkEl[0].src.replace("170x170", "1200x1200");
+
 			console.log('{ "url": "' + bigArtURL + '" }');
 			process.exit(0);
 		} else {
 			console.error("No artwork found on Apple page with width of 170px");
 			process.exit(1);
+		}
+	});
+};
+
+googleapis.discover('customsearch', 'v1').execute( function (err, client) {
+	var request = client.customsearch.cse.list({
+		q: album + ' by ' + artist + ' inurl:album',
+		cx: config.googleSearch.CX_ITUNES_APPLE_COM
+	}).withApiKey(config.googleSearch.API_KEY);
+
+	request.execute( function (err, response) {
+		if (err) {
+			console.error(err);
+			process.exit(1);
+		} else if (!response.items || response.items.length == 0) {
+			console.error("No google results.");
+			process.exit(1);
+		} else {
+			var albumPageURL = response.items[0].link;
+
+			scrapeBigArtwork(albumPageURL);
 		}
 	});
 });
