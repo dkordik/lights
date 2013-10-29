1641e6acbf4c9a6c7c02316e50512dd561aa741a
Dan Kordik <dkordik.public@gmail.com>
2013-10-29 17:34:33 -0500

    Add rgb2hue option to ignore luminosity when setting hue

--- rgb2hue.js
+++ rgb2hue.js
@@ -7,10 +7,10 @@ var api = new HueApi(hostname, username);
 
 var colorConverter = require("./colorconverter").colorConverter();
 
-var arg = process.argv[2];
-if (arg) {
-	// console.log("RGB2HUE, saw: ", arg);
-	rgb = JSON.parse(arg);
+var rgbArg = process.argv[2];
+if (rgbArg) {
+	// console.log("RGB2HUE, saw: ", rgbArg);
+	rgb = JSON.parse(rgbArg);
 } else {
 	console.error("You must provide an rgb value, in quotes: '[0,0,0]'");
 	process.exit(1);
@@ -20,18 +20,22 @@ Array.prototype.max = function() {
   return Math.max.apply(null, this);
 };
 
-// var luminosity = rgb.max();
+var luminosity = rgb.max();
 var color = colorConverter.rgbToXyBri({
 	r: rgb[0]/255, g: rgb[1]/255, b: rgb[2]/255
 });
 color = colorConverter.xyBriForModel(color, 'LCT001');
 
 var state = {
-	// bri: luminosity,
 	xy: [ color.x, color.y ]
 };
 
 var lightGroup = process.argv[3] | 0;
+
+var ignoreLuminosityArg = process.argv[4];
+if (!ignoreLuminosityArg) {
+	state.bri = luminosity;
+}
 
 api.setGroupLightState(lightGroup, state, function (err, lights) {
 	if (err) { console.error("ERROR: ", err); }
